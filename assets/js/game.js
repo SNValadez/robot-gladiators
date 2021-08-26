fbcf5a5fa1e5f7d3fa0144b7fb829e88ec780a29
SNValadez <snvaladez13@gmail.com>
2021-08-26 06:02:45 -0700

    muliple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,17 +14,10 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 console.log(enemyNames);
 
+console.log(enemyNames);
+console.log(enemyNames.length);
 
 
-
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames [i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
-
-// Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
 
 var fight = function(enemyName) {
     while(playerHealth > 0 && enemyHealth > 0) {
@@ -86,7 +79,33 @@ var fight = function(enemyName) {
 // run fight function to start game
 //fight(enemyRobot);
 for (var i = 0; i < enemyNames.length; i++) {
+    // if player is still alive, keep fighting
+    if (playerHealth > 0) {
+        //let player know what round they are in, remember that array start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+    
+    // reset EnemyHealth before starting new fight
     enemyHealth = 50;
+    
+    //use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    }
+    // if the player isn't alive, stop the game
+    else {
+        window.alert("You have lost your robot in battle! Game over!");
+        break;
+    }
+
 }
+
+
+
+   
+    
+    
